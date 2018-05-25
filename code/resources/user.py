e21a65f0a39dffa652eaba942fd751828e39bb14
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-05-25 07:42:33 -0700

    coded a demo of the getuser by email

--- code/resources/user.py
+++ code/resources/user.py
@@ -1,6 +1,12 @@
 from flask import request, jsonify
 from flask_restful import Resource, reqparse
 
+from models.user import UserModel
+
 class User(Resource):
     def get(self, email):
-        pass
+        user = UserModel.find_by_email(email)
+
+        if user is None:
+            return {'success': False, 'message': 'User not found'}, 404
+        return user.json(), 400
