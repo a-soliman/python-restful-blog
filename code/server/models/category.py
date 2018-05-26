df6c66239fa29060025fd4e3047ff895bf4029ea
a-soliman <ahmed.soliman@programmer.net>
2018-05-25 19:46:01 -0700

    coded category model

---
+++ code/server/models/category.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+from db import db
+
+class CategoryModel(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    posts = db.relationship('PostModel', lazy='dynamic')
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "posts": [post.json() for post in self.posts.all()]
+        }
+    
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+    
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+    
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+        return
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+        return
